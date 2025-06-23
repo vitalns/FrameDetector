f80a2c5f5654252f64eaa9b86b042473a82421ac
vitalns <vitalnaz1@gmail.com>
2025-06-24 02:25:59 +0300

    delate all

--- cnn/model_input_info.py
+++
@@ -1,6 +0,0 @@
-from tensorflow.keras.models import load_model
-
-model = load_model('model.h5')
-
-print(f'Входная форма: {model.input_shape}')  # (None, 128, 128, 1)
-print(f'Тип входных данных: {model.input.dtype}')  # float32
